7ea8e6705239d28d02f90f3bfc453a79c4e96f09
Ritik Sharma <sharmaxritik@gmail.com>
2026-06-02 12:06:26 +0530

    fixed render database

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/inventory_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:admin123@localhost:5432/inventory_db"
+)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {}
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
